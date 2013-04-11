c1a134630947dea3f878c84853ad5d4477d8033d
Jan Prachař <jan.prachar@gmail.com>
2013-04-11 15:37:21 +0200

    Prevent empty output from htmlspecialchars.

--- conf/config.php
+++ conf/config.php
@@ -1,7 +1,7 @@
 <?php
 $private = array(
 
-	'version' => 'v1.4.1',
+	'version' => 'v1.4.2',
 
 	// zacatky cest k logum - je to kvuli odstraneni zacatku cesty pro setrideni do skupin, jinak spadne do vychozi
 	'stripPaths' => array('/var/log/'),
