ffdb44c8cf00bf3258858c6d76f8111f40877ade
Roman Ozana <ozana@omdesign.cz>
2013-05-02 23:23:56 +0200

    Better example script

--- conf/example.local.php
+++ conf/example.local.php
@@ -3,11 +3,23 @@
  * @author Roman Ozana <ozana@omdesign.cz>
  */
 $private['filelist'] = array(
-	'/var/log/mongo.log', '/var/log/php.log',
+	'/var/log/mongo.log',
+  '/var/log/php.log',
 	'/Users/roman/workspace/*/*.log',
 	'/Users/roman/workspace/*/*/bluescreen/*.log',
 	'/Users/roman/workspace/*/*/bluescreen/*.html',
 	'/Users/roman/workspace/*/testbuilder/reports/selenium-screens/*.png',
 	'/Users/roman/workspace/*/testbuilder/reports/selenium-screens/*.html',
 );
+
+// or use our example
+
+$private['filelist'] = array(
+	__DIR__ . '/../example/*.log',
+	__DIR__ . '/../example/*.jpg',
+	__DIR__ . '/../example/*.png',
+	__DIR__ . '/../example/*.html',
+);
+
+
 
